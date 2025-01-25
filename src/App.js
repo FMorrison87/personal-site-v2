ec5f33bd36f079862e257afd00ed9ba0f25374b0
Frances Morrison <francesmorrison@Francess-MacBook-Pro.local>
2025-01-25 08:03:32 -0500

    fix: small fixes and typo correction

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
-import React, { useState } from 'react';
-import Nav from "./components/nav/nav"
-import Header from "./components/header/header"
-import CardCollapse from "./components/card-collapse/card-collapse"
+import React, { useState } from "react";
+import Nav from "./components/nav/nav";
+import Header from "./components/header/header";
+import CardCollapse from "./components/card-collapse/card-collapse";
 import "./App.scss";
 
 function App() {
-  const [navOpen, setNavOpen] = useState(false)
+  const [navOpen, setNavOpen] = useState(false);
   return (
     <div className="site-wrapper">
-      <Nav navOpen={navOpen} onClickOutside={() => {setNavOpen(false)} }/>
+      <Nav
+        navOpen={navOpen}
+        onClickOutside={() => {
+          setNavOpen(false);
+        }}
+      />
       <Header />
       <main className="site-main">
         <section id="About">
           <h2>About</h2>
-          <p>As a self-taught developer my background includes a multitude of experiences. I have worked in tech since 2013, in roles that spanned community management, user retention and data analytics, UI and game design, QA and QA management, marketing, and project management.</p>
-          <p>Currently my career has taken me into the military and space industry where I work as a front-end developer supporting Astro UXDS, the UX design system that is being integrated into space applications for the US Space Force. I support the web-components, documentation site, as well as perform audits and re-skinning of space applications for Astro compliance.</p>
+          <p>
+            I am a professional software developer who specializes in
+            large-scale applications with intricate state management and complex
+            data manipulation.
+          </p>
+          <p>
+            I currently work in the aerospace and defense industry with a
+            primary focus on satellite applications.
+          </p>
 
           <div id="Ethos">
             <h3>Ethos</h3>
             <strong>Clean, maintanable code.</strong>
-            <p>I write code that is easy for my co-workers to understand, work in, and extend.</p>
+            <p>
+              I write code that is easy for my co-workers to understand, work
+              in, and extend.
+            </p>
             <strong>Good faith design execution.</strong>
-            <p>I turn professional designs into code that matches the designers intent.</p>
+            <p>
+              I turn professional designs into code that matches the designers
+              intent.
+            </p>
             <strong>Clear, up-front communication</strong>
-            <p>If I encounter an execution problem I am practiced at collaborating with stakeholders to come up with a solution that works for everyone.</p>
+            <p>
+              If I encounter an execution problem I am practiced at
+              collaborating with stakeholders to come up with a solution that
+              works for everyone.
+            </p>
           </div>
           <div id="Technologies">
             <h3>Technologies</h3>
@@ -34,12 +56,17 @@ function App() {
                 <li>CSS</li>
                 <li>SCSS / Sass</li>
                 <li>JavaScript</li>
+                <li>Node.js</li>
                 <li>TypeScript</li>
                 <li>Web Components</li>
                 <li>React</li>
+                <li>Angular</li>
+                <li>RxJs & NgRx</li>
+                <li>REST APIs</li>
               </ul>
               <ul>
                 <li>Stencil.js</li>
+                <li>Lit</li>
                 <li>Astro.js</li>
                 <li>Adobe XD</li>
                 <li>Adobe Photoshop</li>
@@ -52,34 +79,107 @@ function App() {
         </section>
         <section id="Portfolio">
           <h2>Portfolio</h2>
-          <CardCollapse cardTitle={'Astro UXDS'} chipArray={['Web Components','Stencil.js','React','Astro.js','Javascript ES6']} cardDescription={'Part of the team that creates and maintains web components for Astro UX Design System, used in US Space Force Satellite Applications.'} dataActive={false}>
+          <CardCollapse
+            cardTitle={"Astro UXDS"}
+            chipArray={[
+              "Web Components",
+              "Stencil.js",
+              "React",
+              "Astro.js",
+              "Javascript ES6",
+            ]}
+            cardDescription={
+              "Part of the team that creates and maintains web components for Astro UX Design System, used in US Space Force Satellite Applications."
+            }
+            dataActive={false}
+          >
             <div>
               <strong>Highlights</strong>
               <ul>
                 <li>Creation of web components using Stencil.js</li>
-                <li>Close collaboration with design team to execute official Figma designs.</li>
-                <li>Web components adhere to and support the compliance tiers for military applications.</li>
-                <li>Maintenance of documentation site using Astro.js and vanilla web components.</li>
+                <li>
+                  Close collaboration with design team to execute official Figma
+                  designs.
+                </li>
+                <li>
+                  Web components adhere to and support the compliance tiers for
+                  military applications.
+                </li>
+                <li>
+                  Maintenance of documentation site using Astro.js and vanilla
+                  web components.
+                </li>
               </ul>
-              <a href="https://astrouxds.com" target="_blank" rel="noreferrer">Astro UXDS</a>
+              <a href="https://astrouxds.com" target="_blank" rel="noreferrer">
+                Astro UXDS
+              </a>
             </div>
           </CardCollapse>
-          <CardCollapse cardTitle={'Supracoders Space Electronic Warfare Training Application - Astro Redo'} chipArray={['React','Astro Web Components','Astro Design Tokens','JavaScript ES6','Material UI Kit']} cardDescription={'An exercise to bring an existing application into compliance with Astro 7 using a variety of methods.'} dataActive={false}>
+          <CardCollapse
+            cardTitle={
+              "Supracoders Space Electronic Warfare Training Application - Astro Redo"
+            }
+            chipArray={[
+              "React",
+              "Astro Web Components",
+              "Astro Design Tokens",
+              "JavaScript ES6",
+              "Material UI Kit",
+            ]}
+            cardDescription={
+              "An exercise to bring an existing application into compliance with Astro 7 using a variety of methods."
+            }
+            dataActive={false}
+          >
             <div>
               <strong>Highlights</strong>
               <ul>
-                <li>Exercise was undertaken a number of times. First the app was reskinned using Astro design tokens, then web components, then both. Then the application internals were re-worked to improve user experience and Astro compliance.</li>
+                <li>
+                  Exercise was undertaken a number of times. First the app was
+                  reskinned using Astro design tokens, then web components, then
+                  both. Then the application internals were re-worked to improve
+                  user experience and Astro compliance.
+                </li>
                 <li>Undertaken with Astro co-worker Kiley Mitti.</li>
-                <li>Core application was in React with a custom Astro theme using MUI, all of which needed to be pieced apart and re-assembled using Astro.</li>
+                <li>
+                  Core application was in React with a custom Astro theme using
+                  MUI, all of which needed to be pieced apart and re-assembled
+                  using Astro.
+                </li>
                 <li>Original application create by Theodore Kruczek</li>
               </ul>
               <div className="card_link-wrapper">
-                <a href="https://deploy-preview-8--quiet-kheer-2945e7.netlify.app/" target="_blank" rel="noreferrer">Supracoders Training Application - Final Version</a>
-                <a href="https://github.com/RocketCommunicationsInc/supracoders-app/pulls" target="_blank" rel="noreferrer">Full List of Versions - Github</a>
+                <a
+                  href="https://deploy-preview-8--quiet-kheer-2945e7.netlify.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Supracoders Training Application - Final Version
+                </a>
+                <a
+                  href="https://github.com/RocketCommunicationsInc/supracoders-app/pulls"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Full List of Versions - Github
+                </a>
               </div>
             </div>
           </CardCollapse>
-          <CardCollapse cardTitle={'Simos Produce'} chipArray={['Shopify / Liquid','HTML','Sass','jQuery','JavaScript']} cardDescription={'E-Commerce grocery box home delivery website for the Shopify platform.'} dataActive={false}>
+          <CardCollapse
+            cardTitle={"Simos Produce"}
+            chipArray={[
+              "Shopify / Liquid",
+              "HTML",
+              "Sass",
+              "jQuery",
+              "JavaScript",
+            ]}
+            cardDescription={
+              "E-Commerce grocery box home delivery website for the Shopify platform."
+            }
+            dataActive={false}
+          >
             <div>
               <strong>Feature Highlights</strong>
               <ul>
@@ -88,25 +188,57 @@ function App() {
                 <li>Delivery date-picker</li>
                 <li>Custom zone tagging for admin</li>
               </ul>
-              <a href="https://simosproduce.com" target="_blank" rel="noreferrer">Simos Produce</a>
+              <a
+                href="https://simosproduce.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Simos Produce
+              </a>
             </div>
           </CardCollapse>
-          <CardCollapse cardTitle={'Velocio Apparel'} chipArray={['Shopify / Liquid','HTML','SCSS','JavaScript','Tailwind']} cardDescription={'Full rebuild of E-Commerce cycling apparel website on the Shopify platform.'} dataActive={false}>
+          <CardCollapse
+            cardTitle={"Velocio Apparel"}
+            chipArray={[
+              "Shopify / Liquid",
+              "HTML",
+              "SCSS",
+              "JavaScript",
+              "Tailwind",
+            ]}
+            cardDescription={
+              "Full rebuild of E-Commerce cycling apparel website on the Shopify platform."
+            }
+            dataActive={false}
+          >
             <div>
               <strong>Feature Highlights</strong>
               <ul>
-                <li>Improved developer experience with build system and compiler use (Webpack / Babel)</li>
-                <li>Execution of given design for product pages, creation of design based on stylesheet for the rest of the site.</li>
+                <li>
+                  Improved developer experience with build system and compiler
+                  use (Webpack / Babel)
+                </li>
+                <li>
+                  Execution of given design for product pages, creation of
+                  design based on stylesheet for the rest of the site.
+                </li>
                 <li>Migration from jQuery to ES6</li>
-                <li>Redesigned homepage with video hero option and product highlight sections.</li>
+                <li>
+                  Redesigned homepage with video hero option and product
+                  highlight sections.
+                </li>
                 <li>Redone collection page with custom swatch pickers.</li>
-                <li>Custom CMS improvements for Velocio employees working on the website.</li>
+                <li>
+                  Custom CMS improvements for Velocio employees working on the
+                  website.
+                </li>
                 <li>Speed audits using Lighthouse for mobile / desktop.</li>
               </ul>
-              <a href="https://velocio.cc" target="_blank" rel="noreferrer">Velocio Apparel</a>
+              <a href="https://velocio.cc" target="_blank" rel="noreferrer">
+                Velocio Apparel
+              </a>
             </div>
           </CardCollapse>
-          
         </section>
       </main>
     </div>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,22 +1,32 @@
-import ArrowDownIcon from '@mui/icons-material/ArrowCircleDownRounded';
-import "./header.scss"
+import ArrowDownIcon from "@mui/icons-material/ArrowCircleDownRounded";
+import "./header.scss";
 
 function Header() {
-    function jumpToFirstSection() {
-        const firstSection = document.querySelector('.site-main')
-        firstSection.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" })
-    }
-
-    return (
-        <header className="site-header">
-          <div className="site-header_title">
-            <h1>Frances Morrison</h1>
-            <p>Frontend Developer & UX Designer</p>
-            <ArrowDownIcon sx={{ color: 'var(--interactive-color)', fontSize: 50, cursor: 'pointer', }} onClick={jumpToFirstSection} />
-          </div>
-        </header>
-    );
+  function jumpToFirstSection() {
+    const firstSection = document.querySelector(".site-main");
+    firstSection.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
   }
-  
-  export default Header;
-  
+
+  return (
+    <header className="site-header">
+      <div className="site-header_title">
+        <h1>Frances Morrison</h1>
+        <p>Software Developer</p>
+        <ArrowDownIcon
+          sx={{
+            color: "var(--interactive-color)",
+            fontSize: 50,
+            cursor: "pointer",
+          }}
+          onClick={jumpToFirstSection}
+        />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
